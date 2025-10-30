8a197d2fc0411106fff492c219af1e639df86268
Pavel Egorov <epe.mw@mail.ru>
2025-10-30 16:47:05 +0300

    example.py has been fixed

--- example.py
+++ example.py
@@ -4,7 +4,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Welcome to Flask!"
+@app.get("/")
+def hello_get():
+    return "Hello, GET!"
+
+
+@app.post("/")
+def hello_post():
+    return "Hello, POST!"
 
